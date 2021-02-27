a76baba5db9943026107e21b3fb769fe8285fd7d
Magno Ramos <magno23ramos@gmail.com>
2021-02-27 08:46:37 -0300

    setup database in repository

--- sdk/src/main/java/com/app/sdk/repository/TaskRepository.kt
+++ sdk/src/main/java/com/app/sdk/repository/TaskRepository.kt
@@ -3,12 +3,12 @@ package com.app.sdk.repository
 import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
-import com.app.sdk.AppDatabase
+import com.app.sdk.AppDatabaseProvider
 import com.app.sdk.entities.TaskEntity
 import com.app.sdk.models.Task
 
 class TaskRepository(applicationContext: Context) {
-    private val taskDao = AppDatabase.getInstance(applicationContext).taskDao()
+    private val taskDao = AppDatabaseProvider.getInstance(applicationContext).taskDao()
 
     fun getAll(): LiveData<List<Task>> = taskDao.getAll().map {
         it.map { entity -> Task(entity) }
