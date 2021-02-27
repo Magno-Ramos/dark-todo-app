28134c8a60f6c4977c835b6fb6956a41510a7f7e
Magno Ramos <magno23ramos@gmail.com>
2021-02-26 21:57:56 -0300

    fix intro activity

--- app/src/main/java/com/app/darktodoapp/IntroActivity.kt
+++ app/src/main/java/com/app/darktodoapp/IntroActivity.kt
@@ -1,17 +1,31 @@
 package com.app.darktodoapp
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.edit
 import kotlinx.android.synthetic.main.activity_intro.*
+
+const val INTRO_DISPLAYED_KEY = "INTRO_DISPLAYED_KEY"
 
 class IntroActivity : AppCompatActivity(R.layout.activity_intro) {
     override fun onCreate(savedInstanceState: Bundle?) {
+        checkIntroDisplayed()
         super.onCreate(savedInstanceState)
+        btn_continue.setOnClickListener { goToMain() }
+    }
 
-        btn_continue.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }
+    private fun checkIntroDisplayed() {
+        val prefs = getPreferences(Context.MODE_PRIVATE)
+        val displayed = prefs.getBoolean(INTRO_DISPLAYED_KEY, false)
+
+        if (displayed) goToMain()
+        else prefs.edit(commit = true) { putBoolean(INTRO_DISPLAYED_KEY, true) }
+    }
+
+    private fun goToMain() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
