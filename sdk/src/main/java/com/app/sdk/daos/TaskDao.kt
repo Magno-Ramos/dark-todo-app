807f5bc435b12c13cdc4e77e03c4f893d2ae89c1
Magno Ramos <magno23ramos@gmail.com>
2021-02-27 08:47:41 -0300

    organization of methods

--- sdk/src/main/java/com/app/sdk/daos/TaskDao.kt
+++ sdk/src/main/java/com/app/sdk/daos/TaskDao.kt
@@ -9,18 +9,18 @@ internal interface TaskDao {
     @Query("SELECT * FROM TaskEntity")
     fun getAll(): LiveData<List<TaskEntity>>
 
-    @Query("SELECT * FROM TaskEntity")
-    fun getAllNow(): List<TaskEntity>
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun save(task: TaskEntity)
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun saveNow(task: TaskEntity)
 
     @Update
     suspend fun update(task: TaskEntity)
 
     @Delete
     suspend fun delete(task: TaskEntity)
+
+    @Query("SELECT * FROM TaskEntity")
+    fun getAllNow(): List<TaskEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun saveNow(task: TaskEntity)
 }
