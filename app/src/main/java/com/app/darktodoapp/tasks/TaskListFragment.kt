011a002f9fa8813cab31810b70d41b77dca503c7
Magno Ramos <magno23ramos@gmail.com>
2021-03-02 01:14:18 -0300

    improving task adapter

--- app/src/main/java/com/app/darktodoapp/tasks/TaskListFragment.kt
+++ app/src/main/java/com/app/darktodoapp/tasks/TaskListFragment.kt
@@ -9,19 +9,33 @@ import com.app.darktodoapp.helper.observe
 import com.app.darktodoapp.tasks.adapter.TasksAdapter
 import com.app.sdk.repository.TaskRepository
 import kotlinx.android.synthetic.main.fragment_task_list.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class TaskListFragment : Fragment(R.layout.fragment_task_list) {
 
-    private val tasksAdapter = TasksAdapter()
+    private lateinit var repository: TaskRepository
+    private val tasksAdapter = TasksAdapter { task, checked ->
+        GlobalScope.launch {
+            repository.update(task.copy(complete = checked))
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupTasks()
+        setupObserver(view)
+    }
+
+    private fun setupTasks() {
         recycler_tasks.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = tasksAdapter
         }
+    }
 
-        val repository = TaskRepository(view.context.applicationContext)
+    private fun setupObserver(view: View) {
+        repository = TaskRepository(view.context.applicationContext)
         repository.getAll().observe(viewLifecycleOwner, tasksAdapter::submitList)
     }
 }
--- app/src/main/java/com/app/darktodoapp/tasks/adapter/TasksAdapter.kt
+++ app/src/main/java/com/app/darktodoapp/tasks/adapter/TasksAdapter.kt
@@ -5,7 +5,7 @@ import android.text.style.StrikethroughSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RadioButton
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.core.text.set
 import androidx.recyclerview.widget.DiffUtil
@@ -14,16 +14,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.app.darktodoapp.R
 import com.app.sdk.models.Task
 
-class TasksAdapter : ListAdapter<Task, TasksAdapter.TaskAdapterViewHolder>(
-    DIFF_CALLBACK
-) {
+class TasksAdapter(private val checkChangeListener: (Task, Boolean) -> Unit) :
+    ListAdapter<Task, TasksAdapter.TaskAdapterViewHolder>(DIFF_CALLBACK) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskAdapterViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.item_view_task, parent, false)
-        return TaskAdapterViewHolder(
-            view
-        )
+        return TaskAdapterViewHolder(view, checkChangeListener)
     }
 
     override fun onBindViewHolder(holder: TaskAdapterViewHolder, position: Int) {
@@ -32,13 +29,20 @@ class TasksAdapter : ListAdapter<Task, TasksAdapter.TaskAdapterViewHolder>(
         }
     }
 
-    class TaskAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val checkRadio: RadioButton = itemView.findViewById(R.id.checkbox)
+    class TaskAdapterViewHolder(
+        itemView: View,
+        private val checkChangeListener: (Task, Boolean) -> Unit
+    ) : RecyclerView.ViewHolder(itemView) {
+        private val checkRadio: CheckBox = itemView.findViewById(R.id.checkbox)
         private val txtDescription: TextView = itemView.findViewById(R.id.txt_task_description)
         fun bind(task: Task) {
             checkRadio.isChecked = task.complete
             txtDescription.text = task.description
             txtDescription.underlineFromBoolean(task.complete)
+
+            checkRadio.setOnClickListener {
+                checkChangeListener.invoke(task, checkRadio.isChecked)
+            }
         }
     }
 
