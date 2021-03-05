e80d72463211fc70ce94b18f095c4fe266a286c8
Magno Ramos <magno23ramos@gmail.com>
2021-03-05 19:27:24 -0300

    adding extensions

--- app/src/main/java/com/app/darktodoapp/helper/extensions.kt
+++ app/src/main/java/com/app/darktodoapp/helper/extensions.kt
@@ -1,8 +1,15 @@
 package com.app.darktodoapp.helper
 
+import android.content.Context
 import android.text.format.DateFormat
+import android.view.View
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
+import com.google.android.material.snackbar.Snackbar
 import java.util.*
 
 
@@ -10,6 +17,14 @@ fun currentFormattedDate(): CharSequence {
     val date = Calendar.getInstance().time
     return DateFormat.format("EEEE',' dd", date).toString().capitalize()
 }
+
+fun Context.getCompatDrawable(@DrawableRes drawableRes: Int) =
+    ContextCompat.getDrawable(this, drawableRes)
+
+fun Context.getCompatColor(@ColorRes colorRes: Int) =
+    ContextCompat.getColor(this, colorRes)
+
+fun View.snack(@StringRes stringRes: Int) = Snackbar.make(this, stringRes, Snackbar.LENGTH_LONG)
 
 fun <T> LiveData<T>.observe(lifecycleOwner: LifecycleOwner, callback: (T) -> Unit) {
     this.observe(lifecycleOwner, androidx.lifecycle.Observer { callback.invoke(it) })
